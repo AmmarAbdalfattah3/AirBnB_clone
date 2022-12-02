97ee1af2c5135f335de9acc67fb379ad17f2adda
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-12-02 07:23:51 +0000

    city model

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for city model"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class implementation"""
+    state_id = ""
+    name = ""
