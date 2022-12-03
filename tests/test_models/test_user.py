f2e8119750be2bd8e4599a9ef71b4358f7ef90d0
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-12-03 10:37:08 +0000

    Test the 'user' model

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Test file for User class"""
+
+
+
+from models.user import User
+from tests.test_models.test_base_model import TestBaseModel
+
+
+class TestUser(TestBaseModel):
+    """Class to test the 'user' file"""
+
+    def setUp(self):
+        """Set up a two instance before each a test case,
+           In addition to instance variable containing the model name.
+        """
+        self.model = User()
+        self._model2 = User()
+        self._name_class = "User"
